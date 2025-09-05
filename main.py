2865d0e00c5f8dcab373b37acdb18a72b726b24b
Aleksandra <alexanc382@gmail.com>
2025-09-05 12:32:29 +0300

    создана функция def index

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import  HTMLResponse
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+def index(request: Request):  # функция. которая возвращает в браузер html-страницу
+    index_file = os.path.join("static", "index.html")
+    with open(index_file) as f:
+        content = f.read()
+        return HTMLResponse(content)
